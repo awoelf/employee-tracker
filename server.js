2dab1745db4220c7095e5bd047be0ea03037a3bd
awoelf <98348782+awoelf@users.noreply.github.com>
2022-11-04 11:32:28 -0500

    added dotenv

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(express.json());
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: 'root',
-        password: 'xortonryla!',
-        database: 'employees_db'
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     },
     console.log(`Connected to the ${process.env.DB_NAME} database.`)
 );
